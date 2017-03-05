c159e6ce7b602feb48152fce072d201bb2f4b7c1
Klaus Purer <klaus.purer@gmail.com>
2017-03-05 21:44:33 +0100

    feat(parser): Add method to get the line number of a particular key in the structure

--- Tests/ReadTest.php
+++ Tests/ReadTest.php
@@ -31,4 +31,40 @@ EOF;
         $this->assertSame(Yaml::parse($yaml), $yamlComments->getYaml());
         $this->assertSame([12 => '  # Some comment here.'], $yamlComments->getComments());
     }
+
+    /**
+     * Tests retrieving the line number for any given key.
+     */
+    public function testLineNumber()
+    {
+        $yaml = <<<'EOF'
+name: Node
+type: module
+# Messing up line numbers with comments, yay!
+description: 'Allows content to be submitted to the site and displayed on pages.'
+package: Core
+version: VERSION
+core: 8.x
+configure: entity.node_type.collection
+dependencies:
+  - text
+  - drupal:rest
+  - views
+  # Some comment here.
+  - rules
+EOF;
+        $yamlComments = YamlComments::parse($yaml);
+        $this->assertSame(1, $yamlComments->getLineNumber('name'));
+        $this->assertSame(2, $yamlComments->getLineNumber('type'));
+        $this->assertSame(4, $yamlComments->getLineNumber('description'));
+        $this->assertSame(5, $yamlComments->getLineNumber('package'));
+        $this->assertSame(6, $yamlComments->getLineNumber('version'));
+        $this->assertSame(7, $yamlComments->getLineNumber('core'));
+        $this->assertSame(8, $yamlComments->getLineNumber('configure'));
+        $this->assertSame(9, $yamlComments->getLineNumber('dependencies'));
+        $this->assertSame(10, $yamlComments->getLineNumber(['dependencies', 0]));
+        $this->assertSame(11, $yamlComments->getLineNumber(['dependencies', 1]));
+        $this->assertSame(12, $yamlComments->getLineNumber(['dependencies', 2]));
+        $this->assertSame(14, $yamlComments->getLineNumber(['dependencies', 3]));
+    }
 }
--- YamlComments.php
+++ YamlComments.php
@@ -82,6 +82,44 @@ class YamlComments
         return $comments;
     }
 
+    /**
+     * Determines the line number of a given key in the YAML content.
+     *
+     * @param string|array $keyPath
+     *   Either a simple key string for the first level of key/value pairs in
+     *   the YAML or a sequence of keys that should be traversed in a nested
+     *   structure.
+     *
+     * @return int
+     *   The line number where the given key appears.
+     */
+    public function getLineNumber($keyPath)
+    {
+        // Support a simple key path passed as single string.
+        if (!is_array($keyPath)) {
+            $keyPath = [$keyPath];
+        }
+        $line = 0;
+        $structure = $this->yaml;
+        foreach ($keyPath as $key) {
+            foreach ($structure as $currentKey => $value) {
+                $line++;
+                if ($currentKey === $key) {
+                    $structure = $value;
+                    continue 2;
+                }
+            }
+        }
+
+        foreach ($this->comments as $commentLineNr => $comment) {
+            if ($commentLineNr <= $line) {
+                $line++;
+            }
+        }
+
+        return $line;
+    }
+
     public function getYaml()
     {
         return $this->yaml;
